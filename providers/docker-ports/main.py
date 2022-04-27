b2c438b2f562be57384bbc71bcf28509dd8db214
Ivan Bokov <bokov@selectel.ru>
2022-04-27 18:48:30 +0300

    8888

--- providers/docker-ports/main.py
+++ providers/docker-ports/main.py
@@ -20,7 +20,7 @@ class DockerProvider(Provider):
             working_dir=self.working_dir,
             resources=self.resources,
             artifacts=self.artifacts,
-            ports=self.ports
+            ports=[8888]
         )]
 
 
