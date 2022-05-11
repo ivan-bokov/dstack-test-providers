6a33b3f8039abc0daf4e6c7110aaa5d2b9092455
Ivan Bokov <bokov@selectel.ru>
2022-05-11 12:09:21 +0300

    Fix main.py format

--- providers/pytorch-ddp/main.py
+++ providers/pytorch-ddp/main.py
@@ -1,7 +1,16 @@
-from typing import List
+from typing import List, Optional
 import sys
 
-from dstack import Provider, Job
+from dstack import Provider, Job, Resources, Gpu
+
+
+class PytorchResources(Resources):
+    def __init__(self, cpu: Optional[int] = None, memory: Optional[str] = None,
+                 gpu: Optional[Gpu] = None, nodes: Optional[int] = 1):
+        self.cpu = cpu
+        self.memory = memory
+        self.gpu = gpu
+        self.nodes = nodes
 
 
 class PytorchDDPProvider(Provider):
@@ -14,6 +23,39 @@ class PytorchDDPProvider(Provider):
         self.ports = self.workflow.data.get("ports") or []
         self.resources = self.workflow.data.get("resources")
 
+    def check_resoures(self) -> Optional[Resources]:
+        if self.workflow.data.get("resources"):
+            resources = Resources()
+            if self.workflow.data["resources"].get("cpu"):
+                if not str(self.workflow.data["resources"]["cpu"]).isnumeric():
+                    sys.exit("resources.cpu in workflows.yaml should be an integer")
+                cpu = int(self.workflow.data["resources"]["cpu"])
+                if cpu > 0:
+                    resources.cpu = cpu
+            if self.workflow.data["resources"].get("nodes"):
+                if not str(self.workflow.data["resources"]["nodes"]).isnumeric():
+                    sys.exit("resources.nodes in workflows.yaml should be an integer")
+                nodes = int(self.workflow.data["resources"]["nodes"])
+                if nodes > 0:
+                    resources.nodes = nodes
+            if self.workflow.data["resources"].get("memory"):
+                resources.memory = self.workflow.data["resources"]["memory"]
+            if str(self.workflow.data["resources"].get("gpu")).isnumeric():
+                gpu = int(self.workflow.data["resources"]["gpu"])
+                if gpu > 0:
+                    resources.gpu = Gpu(gpu)
+            for resource_name in self.workflow.data["resources"]:
+                if resource_name.endswith("/gpu") and len(resource_name) > 4:
+                    if not str(self.workflow.data["resources"][resource_name]).isnumeric():
+                        sys.exit(f"resources.'{resource_name}' in workflows.yaml should be an integer")
+                    gpu = int(self.workflow.data["resources"][resource_name])
+                    if gpu > 0:
+                        resources.gpu = Gpu(gpu, name=resource_name[:-4])
+            if resources.cpu or resources.memory or resources.gpu:
+                return resources
+            else:
+                return None
+
     def create_jobs(self) -> List[Job]:
         masterJob = Job(
             image=self.image,
@@ -24,11 +66,8 @@ class PytorchDDPProvider(Provider):
             ports=self.ports
         )
         jobs = [masterJob]
-        if self.resources.get("nodes"):
-            if not str(self.resources.get("nodes")).isnumeric():
-                sys.exit("resources.nodes in workflows.yaml should be an integer")
-            nodes = int(self.resources.get("nodes"))
-            for i in range(nodes - 1):
+        if self.resources.nodes > 1:
+            for i in range(self.resources.nodes - 1):
                 jobs.append(Job(
                     image=self.image,
                     commands=self.commands,
